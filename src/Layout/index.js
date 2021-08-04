0c663e632d27102e50465238e03c7b15f6ae448c
Kevin Zuniga <kzunigac@uvm.edu>
2021-08-03 22:41:10 -0700

    added footer

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
       <Logo />
       <BrowserBar />
       <div className='container mx-auto py-2'>{children}</div>
+      <div>footer</div>
     </div>
   )
 }

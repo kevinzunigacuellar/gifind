6e9fa20ec86522ed2ae2b03aac283e90a4b7f549
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-05-31 12:07:20 -0400

    add placeholder and offset darkmode script

--- src/components/TrendingGifs.tsx
+++ src/components/TrendingGifs.tsx
@@ -1,13 +1,13 @@
-import { createResource, For } from 'solid-js'
+import { createResource, For, Suspense } from 'solid-js'
 import Subtitle from '../components/Subtitle'
 
 const fetchTrendingGifs = async () => {
   const res = await fetch(
-    'https://api.giphy.com/v1/gifs/trending?api_key=kVePfJ4smdAF1OyaGHxq8LDms0aoXC0o&limit=20'
+    `https://api.giphy.com/v1/gifs/trending?api_key=${import.meta.env.VITE_API_KEY}&limit=20`
   )
   const { data } = await res.json()
   const trendingGifs = data.map(({ id, images }) => {
-    const { webp, width, height } = images.fixed_height
+    const { webp, width, height } = images.fixed_height_downsampled
     return {
       id,
       url: webp,
@@ -24,19 +24,33 @@ export default function TrendingGifs() {
   return (
     <section class="mt-8">
       <Subtitle text="🚀 Trending gifs" />
-      <div class="flex gap-2 overflow-x-auto snap-x snap-mandatory rounded-lg overflow-hidden">
-        <For each={trendingGifs()}>
-          {gif => (
-            <img
-              src={gif.url}
-              alt={gif.id}
-              width={gif.width}
-              height={gif.height}
-              class="rounded-lg snap-end"
-            />
-          )}
-        </For>
+      <div class="flex gap-2 overflow-x-auto rounded-lg overflow-hidden">
+        <Suspense fallback={<TrendingGifsPlaceholder />}>
+          <For each={trendingGifs()}>
+            {gif => (
+              <img
+                src={gif.url}
+                alt={gif.id}
+                width={gif.width}
+                height={gif.height}
+                class="rounded-lg"
+                loading="lazy"
+              />
+            )}
+          </For>
+        </Suspense>
       </div>
     </section>
+  )
+}
+
+function TrendingGifsPlaceholder() {
+  return (
+    <>
+      <div class="h-[200px] w-60 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-[200px] w-60 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-[200px] w-60 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-[200px] w-60 bg-slate-300 rounded-lg animate-pulse"></div>
+    </>
   )
 }
--- src/components/TrendingSearches.tsx
+++ src/components/TrendingSearches.tsx
@@ -1,9 +1,9 @@
-import { createResource, For } from 'solid-js'
+import { createResource, For, Suspense } from 'solid-js'
 import Subtitle from './Subtitle'
 
 const fetchTrendingSearches = async () => {
   const res = await fetch(
-    'https://api.giphy.com/v1/trending/searches?api_key=kVePfJ4smdAF1OyaGHxq8LDms0aoXC0o'
+    `https://api.giphy.com/v1/trending/searches?api_key=${import.meta.env.VITE_API_KEY}`
   )
   const { data } = await res.json()
   return data
@@ -16,17 +16,38 @@ export default function TrendingSearches() {
     <section class="mt-6">
       <Subtitle text="🎉 Trending searches" />
       <div class="flex gap-2 flex-wrap">
-        <For each={trendingSearches()}>
-          {search => (
-            <a
-              href={`/search/${search}`}
-              class="inline-block px-4 py-1 border text-zinc-500 hover:shadow dark:hover:shadow-black/50 dark:border-zinc-800 dark:text-zinc-500 dark:hover:text-zinc-300 hover:text-zinc-900 rounded-lg"
-            >
-              {search}
-            </a>
-          )}
-        </For>
+        <Suspense fallback={<TrendingSearchesPlaceholder />}>
+          <For each={trendingSearches()}>
+            {search => (
+              <a
+                href={`/search/${search}`}
+                class="inline-block px-4 py-1 border text-zinc-500 hover:shadow dark:hover:shadow-black/50 dark:border-zinc-800 dark:text-zinc-500 dark:hover:text-zinc-300 hover:text-zinc-900 rounded-lg"
+              >
+                {search}
+              </a>
+            )}
+          </For>
+        </Suspense>
       </div>
     </section>
+  )
+}
+
+function TrendingSearchesPlaceholder() {
+  return (
+    <>
+      <div class="h-9 w-32 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-60 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-48 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-32 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-60 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-48 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-60 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-32 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-32 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-48 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-60 bg-slate-300 rounded-lg animate-pulse"></div>
+      <div class="h-9 w-48 bg-slate-300 rounded-lg animate-pulse"></div>
+    </>
   )
 }
